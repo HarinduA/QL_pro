7b2626e5cd1db28a1999c0021a2df92a77fb6d6b
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-07-09 22:44:45 +0530

    Add files via upload

---
+++ pages/HomePage.js
@@ -0,0 +1,17 @@
+import JobList from '../components/JobList';
+import { jobs } from '../lib/fake-data';
+import { getJobs } from '../lib/graphql/queries';
+
+getJobs().then((jobs) => console.log('jobs:', jobs));
+function HomePage() {
+  return (
+    <div>
+      <h1 className="title">
+        Job Board
+      </h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default HomePage;
